0c4d61fb203eacaa1c33eda12902ab0df0962185
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-04-04 22:04:08 -0700

    chore: use ipni libp2p config

--- src/sw.ts
+++ src/sw.ts
@@ -30,7 +30,7 @@ self.addEventListener('activate', event => {
   console.log('sw activating')
   void (async () => {
     // helia = await getHelia({ libp2pConfigType: 'dht', usePersistentDatastore: true })
-    helia = await getHelia({ libp2pConfigType: 'bootstrapOnly', usePersistentDatastore: true })
+    helia = await getHelia({ libp2pConfigType: 'ipni', usePersistentDatastore: true })
     // helia = await getHelia({ libp2pConfigType: 'ipni', usePersistentDatastore: false })
   })()
 })
@@ -38,7 +38,7 @@ self.addEventListener('activate', event => {
 const fetchHandler = async ({ url, request }: { url: URL, request: Request }): Promise<Response> => {
   if (helia == null) {
     // helia = await getHelia({ libp2pConfigType: 'dht', usePersistentDatastore: true })
-    helia = await getHelia({ libp2pConfigType: 'bootstrapOnly', usePersistentDatastore: true })
+    helia = await getHelia({ libp2pConfigType: 'ipni', usePersistentDatastore: true })
     // helia = await getHelia({ libp2pConfigType: 'ipni', usePersistentDatastore: false })
   } else {
     // await helia.start()
