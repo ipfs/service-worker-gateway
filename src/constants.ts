0434b86c40397e4545ea68f72f729fe39e048eac
Marco Munizaga <git@marcopolo.io>
2023-04-15 19:48:55 +0200

    Squashed 'vendor/ipfs-bitswap/' content from commit 9a8bbf7
    git-subtree-dir: vendor/ipfs-bitswap
    git-subtree-split: 9a8bbf792d5246cec7b83ca5d6bf29fa0e3b67cd

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+const SECOND = 1000
+
+export const maxProvidersPerRequest = 3
+export const providerRequestTimeout = 10 * SECOND
+export const hasBlockTimeout = 15 * SECOND
+export const provideTimeout = 15 * SECOND
+export const kMaxPriority = Math.pow(2, 31) - 1
+export const maxListeners = 1000
+export const wantlistSendDebounceMs = 1
