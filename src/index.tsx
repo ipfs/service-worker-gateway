8696f2ba7cb49bc86607d4cb721668b39cb7c674
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-09 19:52:25 -0800

    fix: cleanup app entry

--- src/index.tsx
+++ src/index.tsx
@@ -17,51 +17,30 @@ const container = document.getElementById('root')
 const sw = await navigator.serviceWorker.register(new URL('sw.ts', import.meta.url))
 const root = ReactDOMClient.createRoot(container)
 
-// eslint-disable-next-line no-console
-console.log('window.location: ', window.location)
-// get the subdomain from the window.location, minus the BASE_URL
 const subdomain = window.location.hostname.replace(`.${BASE_URL}`, '')
-// eslint-disable-next-line no-console
-console.log('subdomain: ', subdomain)
-
 const subdomainRegex = /^(?<origin>[^/]+)\.(?<protocol>ip[fn]s)?$/
 const subdomainMatch = subdomain.match(subdomainRegex)
-// eslint-disable-next-line no-console
-console.log('subdomainMatch: ', subdomainMatch)
 
-// get urlSearchParameters and check for existing helia-sw parameter & value
-const urlSearchParams = new URLSearchParams(window.location.search)
-const heliaSwPath = urlSearchParams.get('helia-sw')
-// const heliaSwSubdomain = urlSearchParams.get('helia-sw-subdomain')
-if (heliaSwPath != null) {
+const isPathBasedRequest = window.location.pathname.startsWith('/ip')
+const isSubdomainRequest = subdomainMatch?.groups != null
+
+if ((isPathBasedRequest || isSubdomainRequest)) {
+  // SW did not trigger for this request, so show redirect and redirect to the same URL
   root.render(
     <RedirectPage />
   )
-  // if heliaSwPath value is set, then we need to redirect to the path based helia-sw
-  // e.g. http://localhost:3000/?helia-sw=/ipns/docs.ipfs.tech should redirect to http://localhost:3000/ipns/docs.ipfs.tech
-  const pathname = heliaSwPath[0] === '/' ? heliaSwPath : `/${heliaSwPath}`
-  const redirectURL = new URL(pathname, window.location.origin)
-  window.location.replace(redirectURL.toString())
-} else if (subdomainMatch?.groups != null) {
-  // request was not intercepted by service worker, so we need to trigger that.
-
-  // first, render the redirect page.
-  root.render(
-    <RedirectPage />
-  )
-  // if we have a subdomain, then we need to redirect to the subdomain based helia-sw
-  // const { origin, protocol } = subdomainMatch.groups
-  // const pathname = `?helia-sw-subdomain=/${protocol}/${origin}`
-  // const contentUrl = new URL(pathname, window.location.origin)
-  // window.location.replace(contentUrl.toString())
   window.location.replace(window.location.href)
 } else {
-  // we don't have a helia-sw query parameter, so let's render the demo app
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
+  if (window.location.pathname !== '/') {
+    // pathname is not blank, but is invalid. redirect to the root
+    window.location.replace('/')
+  } else {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    )
+  }
 }
 
 // always update the service worker
