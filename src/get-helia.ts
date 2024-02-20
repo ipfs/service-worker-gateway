0c1001014f606c201b619c9386e115b7ab81f72f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-20 12:04:12 -0800

    feat: use idb block&data stores

--- src/get-helia.ts
+++ src/get-helia.ts
@@ -1,17 +1,21 @@
 import { trustlessGateway } from '@helia/block-brokers'
 import { createHeliaHTTP } from '@helia/http'
 import { delegatedHTTPRouting } from '@helia/routers'
-import { LevelBlockstore } from 'blockstore-level'
-import { LevelDatastore } from 'datastore-level'
+import { IDBBlockstore } from 'blockstore-idb'
+import { IDBDatastore } from 'datastore-idb'
 import { getConfig } from './lib/config-db'
 import type { Helia } from '@helia/interface'
 
 export async function getHelia (): Promise<Helia> {
   const config = await getConfig()
+  const blockstore = new IDBBlockstore('./helia-sw/blockstore')
+  const datastore = new IDBDatastore('./helia-sw/datastore')
+  await blockstore.open()
+  await datastore.open()
 
   const helia = await createHeliaHTTP({
-    blockstore: new LevelBlockstore('./blockstore'),
-    datastore: new LevelDatastore('./datastore'),
+    blockstore,
+    datastore,
     blockBrokers: [
       trustlessGateway({
         gateways: ['https://cloudflare-ipfs.com', 'https://dweb.link', 'https://trustless-gateway.dev', ...config.gateways]
