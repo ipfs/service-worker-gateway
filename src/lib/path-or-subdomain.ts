d210ddf003e05c27d8b27a8dbde8a276957cc405
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-20 16:25:55 -0800

    test: shore-up path-or-subdomain check

--- src/lib/path-or-subdomain.ts
+++ src/lib/path-or-subdomain.ts
@@ -1,9 +1,12 @@
+const subdomainRegex = /^(?<origin>[^/]+)\.(?<protocol>ip[fn]s)?$/
+const pathRegex = /^\/(?<protocol>ip[fn]s)\/(?<path>.*)$/
+
 export const isPathOrSubdomainRequest = (baseUrl: string, location: Pick<Location, 'hostname' | 'pathname'>): boolean => {
   const subdomain = location.hostname.replace(`.${baseUrl}`, '')
-  const subdomainRegex = /^(?<origin>[^/]+)\.(?<protocol>ip[fn]s)?$/
   const subdomainMatch = subdomain.match(subdomainRegex)
 
-  const isPathBasedRequest = location.pathname.startsWith('/ip')
+  const pathMatch = location.pathname.match(pathRegex)
+  const isPathBasedRequest = pathMatch?.groups != null
   const isSubdomainRequest = subdomainMatch?.groups != null
 
   return isPathBasedRequest || isSubdomainRequest
