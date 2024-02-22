f80b2ed8d597d9ec16a25d5db40d2d986ff9df7f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-21 18:18:06 -0800

    chore: change gear color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,8 @@ export default function Header (): JSX.Element {
       </a>
 
       <button onClick={() => { setConfigExpanded(!isConfigExpanded) }} style={{ border: 'none', position: 'absolute', top: '0.5rem', right: '0.5rem', background: 'none', cursor: 'pointer' }}>
-        <img alt='Config gear icon' src={gearIcon} style={{ height: 50 }} className='v-top' />
+        {/* https://isotropic.co/tool/hex-color-to-css-filter/ to #ffffff */}
+        <img alt='Config gear icon' src={gearIcon} style={{ height: 50, filter: 'invert(100%) sepia(100%) saturate(0%) hue-rotate(275deg) brightness(103%) contrast(103%)' }} className='v-top' />
       </button>
     </header>
   )
