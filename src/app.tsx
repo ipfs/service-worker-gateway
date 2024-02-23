ff7bafe526a3768a3e242fa029802f50f758d754
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-23 09:55:09 -0800

    chore: consolidate app logic

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,21 @@ import React, { useContext } from 'react'
 import Config from './components/config.tsx'
 import { ConfigContext } from './context/config-context.tsx'
 import HelperUi from './helper-ui.tsx'
+import { isPathOrSubdomainRequest } from './lib/path-or-subdomain.ts'
+import { BASE_URL } from './lib/webpack-constants.ts'
+import RedirectPage from './redirectPage.tsx'
 
 function App (): JSX.Element {
   const { isConfigExpanded, setConfigExpanded } = useContext(ConfigContext)
   if (window.location.pathname === '/config') {
     setConfigExpanded(true)
+  }
+  if (window.location.pathname === '/config') {
+    return <Config />
+  }
+
+  if (isPathOrSubdomainRequest(BASE_URL, window.location)) {
+    return (<RedirectPage />)
   }
 
   if (isConfigExpanded) {
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOMClient from 'react-dom/client'
 import './app.css'
 import App from './app.tsx'
-import Config from './components/config.tsx'
 import { ConfigProvider } from './context/config-context.tsx'
 import { ServiceWorkerProvider } from './context/service-worker-context.tsx'
 import { loadConfigFromLocalStorage } from './lib/config-db.ts'
-import { isPathOrSubdomainRequest } from './lib/path-or-subdomain.ts'
-import { BASE_URL } from './lib/webpack-constants.ts'
-import RedirectPage from './redirectPage.tsx'
 
 await loadConfigFromLocalStorage()
 
@@ -17,35 +13,12 @@ const container = document.getElementById('root')
 
 const root = ReactDOMClient.createRoot(container)
 
-if (window.location.pathname === '/config') {
-  root.render(
-    <React.StrictMode>
-      <ServiceWorkerProvider>
-        <ConfigProvider expanded={true}>
-          <Config />
-        </ConfigProvider>
-      </ServiceWorkerProvider>
-    </React.StrictMode>
-  )
-} else if (isPathOrSubdomainRequest(BASE_URL, window.location)) {
-  // but the requested path is something it should, so show redirect and redirect to the same URL
-  root.render(
-    <React.StrictMode>
-      <ServiceWorkerProvider>
-        <ConfigProvider>
-            <RedirectPage />
-        </ConfigProvider>
-      </ServiceWorkerProvider>
-    </React.StrictMode>
-  )
-} else {
-  root.render(
-    <React.StrictMode>
-      <ServiceWorkerProvider>
-        <ConfigProvider>
-            <App />
-        </ConfigProvider>
-      </ServiceWorkerProvider>
-    </React.StrictMode>
-  )
-}
+root.render(
+  <React.StrictMode>
+    <ServiceWorkerProvider>
+      <ConfigProvider expanded={window.location.pathname === '/config'}>
+          <App />
+      </ConfigProvider>
+    </ServiceWorkerProvider>
+  </React.StrictMode>
+)
