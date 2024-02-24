a9d25236cfe51eecd3866f1763570f309a126f72
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-23 16:50:06 -0800

    fix: dnsLinkLabel decode encode

---
+++ tests/dns-link-labels.spec.ts
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+import { expect } from 'aegir/chai'
+import { dnsLinkLabelDecoder, dnsLinkLabelEncoder } from '../src/lib/dns-link-labels.ts'
+
+describe('dns-link-labels', () => {
+  it('should support specs-ipfs-tech', () => {
+    const actualDomainName = 'specs.ipfs.tech'
+    const encodedLabel = dnsLinkLabelEncoder(actualDomainName)
+    expect(encodedLabel).to.equal('specs-ipfs-tech')
+    const result = dnsLinkLabelDecoder(encodedLabel)
+    expect(result).to.equal(actualDomainName)
+  })
+
+  it('should support en.wikipedia-on-ipfs.org', () => {
+    const actualDomainName = 'en.wikipedia-on-ipfs.org'
+    const encodedLabel = dnsLinkLabelEncoder(actualDomainName)
+    expect(encodedLabel).to.equal('en-wikipedia--on--ipfs-org')
+    const result = dnsLinkLabelDecoder(encodedLabel)
+    expect(result).to.equal(actualDomainName)
+  })
+})
