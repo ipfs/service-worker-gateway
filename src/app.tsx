4fb83575b2c20d61e6c9a0ba3c7d6bda61c4c7a2
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-28 13:23:55 -0700

    fix: first load for new ipns site redirects properly

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Config from './components/config.tsx'
 import { ConfigContext } from './context/config-context.tsx'
 import HelperUi from './helper-ui.tsx'
 import { isConfigPage } from './lib/is-config-page.ts'
-import { isPathOrSubdomainRequest } from './lib/path-or-subdomain.ts'
+import { isPathOrSubdomainRequest, findOriginIsolationRedirect } from './lib/path-or-subdomain.ts'
 import RedirectPage from './redirectPage.tsx'
 
 function App (): JSX.Element {
   const { isConfigExpanded, setConfigExpanded } = useContext(ConfigContext)
+
+  useEffect(() => {
+    async function originEnforcement (): Promise<void> {
+      // enforce  early when loaded before SW was registered
+      const originRedirect = await findOriginIsolationRedirect(window.location)
+      if (originRedirect !== null) {
+        window.location.replace(originRedirect)
+      }
+    }
+    void originEnforcement()
+  }, [])
+
   if (isConfigPage()) {
     setConfigExpanded(true)
     return <Config />
