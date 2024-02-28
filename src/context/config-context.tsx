59aca7eace087a02f922c0258345d66d3ab7d87d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-28 12:39:17 -0700

    chore: config page check supports hash routing

--- src/context/config-context.tsx
+++ src/context/config-context.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { isConfigPage } from '../lib/is-config-page.ts'
 
 const isLoadedInIframe = window.self !== window.top
 
@@ -9,7 +10,7 @@ export const ConfigContext = createContext({
 
 export const ConfigProvider = ({ children, expanded = isLoadedInIframe }: { children: JSX.Element[] | JSX.Element, expanded?: boolean }): JSX.Element => {
   const [isConfigExpanded, setConfigExpanded] = useState(expanded)
-  const isExplicitlyLoadedConfigPage = window.location.pathname === '/config'
+  const isExplicitlyLoadedConfigPage = isConfigPage()
 
   const setConfigExpandedWrapped = (value: boolean): void => {
     if (isLoadedInIframe || isExplicitlyLoadedConfigPage) {
