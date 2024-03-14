47b8af4f433454e20c833b45feaebcd2c7b60a2d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-14 13:40:02 -0700

    fix: css styles applied & config page collapse (#112)
    * tmp
    
    * fix: css styles are included now
    
    * chore: temporary test of assets while I finagle webpack config
    
    * chore: chunk css styles
    
    * chore: reduce changes to minimal necessary
    
    * fix: config initially collapsed in iframe only
    
    * chore: pr comment - update webpack.config.js

--- test/node.js
+++ test/node.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 import { constants } from 'node:fs'
-import { access } from 'node:fs/promises'
+import { access, readFile } from 'node:fs/promises'
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { expect } from 'aegir/chai'
@@ -13,5 +13,12 @@ describe('verify-dist', async () => {
    * test to confirm that the service worker generated in /dist does not change names
    */
     await expect(access(resolve(cwd, '../dist/ipfs-sw-sw.js'), constants.F_OK)).to.not.be.rejected()
+  })
+
+  it('has css file with expected content', async () => {
+    await expect(access(resolve(cwd, '../dist/ipfs-sw-styles.css'), constants.F_OK)).to.not.be.rejected()
+    const contents = await readFile(resolve(cwd, '../dist/ipfs-sw-styles.css'), 'utf8')
+    expect(contents).to.include('47vh')
+    expect(contents).to.include('.local-storage-toggle input.status')
   })
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
 import webpack from 'webpack'
 import BundleAnalyzerPlugin from 'webpack-bundle-analyzer'
@@ -20,6 +21,13 @@ const splitChunks = {
       test: /[\\/]node_modules[\\/]/,
       priority: -10,
       chunks: 'all'
+    },
+    styles: {
+      minChunks: 1,
+      name: 'styles',
+      type: 'css/mini-extract',
+      chunks: 'initial',
+      enforce: true
     },
     sw: {
       test: /[\\/]src[\\/]sw.ts/,
@@ -203,6 +211,11 @@ const common = {
 
     new webpack.DefinePlugin({
       window: 'globalThis' // attempt to naively replace all "window" keywords with "globalThis"
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'ipfs-sw-[name].css',
+      chunkFilename: 'ipfs-sw-[id].css'
     })
   ],
 
@@ -237,8 +250,13 @@ const common = {
 
       // Fonts and SVGs: Inline files
       { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
-
-      { test: /\.(css)$/, use: ['style-loader', 'css-loader'] }
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
     ]
   },
 
