7387c59001778c96dce5fb742238cb3e1a5bfff7
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-22 13:39:47 -0700

    test: path-routing asserts response and headers

--- test-e2e/path-routing.test.ts
+++ test-e2e/path-routing.test.ts
@@ -6,7 +6,14 @@ test.describe('path-routing', () => {
     await page.goto('http://127.0.0.1:3000', { waitUntil: 'networkidle' })
     // wait for service worker to load
     await waitForServiceWorker(page)
-    await page.goto('http://127.0.0.1:3000/ipfs/bafkqablimvwgy3y', { waitUntil: 'networkidle' })
+    const response = await page.goto('http://127.0.0.1:3000/ipfs/bafkqablimvwgy3y', { waitUntil: 'networkidle' })
+
+    expect(response?.status()).toBe(200)
+
+    const headers = await response?.allHeaders()
+
+    expect(headers?.['content-type']).toBe('text/html')
+    expect(headers?.['cache-control']).toBe('public, max-age=29030400, immutable')
 
     // expect page content to contain 'hello'
     const text = await page.textContent('body')
