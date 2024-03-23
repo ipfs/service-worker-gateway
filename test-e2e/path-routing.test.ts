89b3ace21ba51a705a0625d04df797c504d154f8
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-22 19:31:20 -0700

    test: path-routing of identity CID (#146)
    * test: path-routing of identity CID
    
    * test: path-routing asserts response and headers

---
+++ test-e2e/path-routing.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test'
+import { waitForServiceWorker } from './fixtures/wait-for-service-worker.js'
+
+test.describe('path-routing', () => {
+  test('can load identity CID via path', async ({ page }) => {
+    await page.goto('http://127.0.0.1:3000', { waitUntil: 'networkidle' })
+    // wait for service worker to load
+    await waitForServiceWorker(page)
+    const response = await page.goto('http://127.0.0.1:3000/ipfs/bafkqablimvwgy3y', { waitUntil: 'networkidle' })
+
+    expect(response?.status()).toBe(200)
+
+    const headers = await response?.allHeaders()
+
+    expect(headers?.['content-type']).toBe('text/html')
+    expect(headers?.['cache-control']).toBe('public, max-age=29030400, immutable')
+
+    // expect page content to contain 'hello'
+    const text = await page.textContent('body')
+    expect(text).toContain('hello')
+  })
+})
