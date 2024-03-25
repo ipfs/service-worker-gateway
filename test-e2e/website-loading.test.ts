f200541c2afd14272d916e9373845b300acac032
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-25 14:34:23 -0700

    test: add website index.html test

---
+++ test-e2e/website-loading.test.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test'
+import { createKuboNode } from './fixtures/create-kubo-node.js'
+import { loadFixtureDataCar } from './fixtures/load-fixture-data.js'
+import { setConfig } from './fixtures/set-sw-config.js'
+import { waitForServiceWorker } from './fixtures/wait-for-service-worker.js'
+import type { ConfigDb } from '../src/lib/config-db.js'
+import type { Controller } from 'ipfsd-ctl'
+
+test.describe('website-loading', () => {
+  let controller: Controller<'go'>
+  let config: Partial<ConfigDb>
+
+  test.beforeAll(async () => {
+    controller = await createKuboNode()
+    await controller.start()
+    await loadFixtureDataCar(controller, 'gateway-conformance-fixtures.car')
+    config = {
+      gateways: [`http://${controller.api.gatewayHost}:${controller.api.gatewayPort}`],
+      routers: [`http://${controller.api.gatewayHost}:${controller.api.gatewayPort}`]
+    }
+  })
+
+  test.beforeEach(async ({ page }) => {
+    // not testing subdomain redirection here
+    await page.goto('http://127.0.0.1:3000', { waitUntil: 'networkidle' })
+    await setConfig({ page, config })
+    await waitForServiceWorker(page)
+  })
+
+  // TODO: fix https://github.com/ipfs-shipyard/helia-service-worker-gateway/issues/62
+  test.skip('ensure unixfs directory trailing slash is added', async ({ page }) => {
+    const response = await page.goto('http://127.0.0.1:3000/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q', { waitUntil: 'commit' })
+
+    // await page.waitForURL('http://127.0.0.1:3000/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q/')
+    expect(response?.status()).toBe(200)
+
+    const headers = await response?.allHeaders()
+    expect(headers?.location).toBe('http://localhost:3000/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q/')
+
+    expect(response?.url()).toBe('http://127.0.0.1:3000/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q/')
+  })
+
+  test('ensure that index.html is returned for the root path', async ({ page }) => {
+    const response = await page.goto('http://127.0.0.1:3000/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q/')
+
+    expect(response?.status()).toBe(200)
+    const headers = await response?.allHeaders()
+
+    expect(headers?.['content-type']).toContain('text/html')
+    expect(headers?.['cache-control']).toBe('public, max-age=29030400, immutable')
+
+    const bodyBuffer = await response?.body()
+    expect(bodyBuffer?.byteLength).toBe(6)
+    const bodyText = await response?.text()
+    expect(bodyText).toBe('hello\n')
+  })
+})
