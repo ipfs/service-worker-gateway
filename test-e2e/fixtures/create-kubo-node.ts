4304333846c1af45fb7375adcc14768b439eb473
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-25 16:05:19 -0700

    fix: fix unixfs directory redirects (#156)
    * test: add reverse-proxy for e2e tests
    
    * test: subdomain-detection test
    
    * fix: do not render redirectPage if not parent page
    
    * test: subdomain-detection with autoreload test
    
    * chore: spacing lint fix
    
    * chore: address my own pr comments
    
    * test: add website index.html test
    
    * deps: @helia/verified-fetch@1.3.2
    
    * fix: don't manipulate given url
    
    * test: re-enable trailing slash redirect test

---
+++ test-e2e/fixtures/create-kubo-node.ts
@@ -0,0 +1,31 @@
+import { createController, type Controller } from 'ipfsd-ctl'
+import { path as kuboPath } from 'kubo'
+import * as kuboRpcClient from 'kubo-rpc-client'
+
+export async function createKuboNode (): Promise<Controller> {
+  return createController({
+    kuboRpcModule: kuboRpcClient,
+    ipfsBin: kuboPath(),
+    test: true,
+    ipfsOptions: {
+      config: {
+        Addresses: {
+          Swarm: [
+            '/ip4/0.0.0.0/tcp/4001',
+            '/ip4/0.0.0.0/tcp/4002/ws'
+          ],
+          Gateway: '/ip4/127.0.0.1/tcp/8180'
+        },
+        Gateway: {
+          NoFetch: true,
+          ExposeRoutingAPI: true,
+          HTTPHeaders: {
+            'Access-Control-Allow-Origin': ['*'],
+            'Access-Control-Allow-Methods': ['GET', 'POST', 'PUT', 'OPTIONS']
+          }
+        }
+      }
+    },
+    args: ['--enable-pubsub-experiment', '--enable-namesys-pubsub']
+  })
+}
