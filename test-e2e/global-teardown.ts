9874354bd55a3e5554db26a6e9f9121b292c6902
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-26 14:39:46 -0700

    test: e2e test for hamt-sharded directory content (#157)
    * test: e2e test for hamt-sharded directory content
    
    * chore: update hamt-dir test & kubo handling

--- test-e2e/global-teardown.ts
+++ test-e2e/global-teardown.ts
@@ -3,5 +3,13 @@ import { $ } from 'execa'
 import { kuboRepoDir } from './fixtures/load-kubo-fixtures.js'
 
 export default async function globalTeardown (config: Config): Promise<void> {
+  const kuboPid = process.env.KUBO_PID
+  if (kuboPid != null) {
+    try {
+      await $`kill -9 ${kuboPid}`
+    } catch {
+      // ignore
+    }
+  }
   await $`rm -rf ${kuboRepoDir}`
 }
---
+++ test-e2e/hamt-dir.test.ts
@@ -0,0 +1,16 @@
+import { testPathRouting as test, expect } from './fixtures/config-test-fixtures.js'
+
+test.describe('hamt-dir', () => {
+  test('can open UnixFS file from HAMT-sharded directory', async ({ page }) => {
+    const response = await page.goto('http://127.0.0.1:3333/ipfs/bafybeidbclfqleg2uojchspzd4bob56dqetqjsj27gy2cq3klkkgxtpn4i/685.txt')
+
+    expect(response?.status()).toBe(200)
+    const headers = await response?.allHeaders()
+
+    expect(headers?.['content-type']).toContain('text/plain')
+    expect(headers?.['cache-control']).toBe('public, max-age=29030400, immutable')
+
+    const bodyBuffer = await response?.body()
+    expect(bodyBuffer?.byteLength).toBe(1026)
+  })
+})
