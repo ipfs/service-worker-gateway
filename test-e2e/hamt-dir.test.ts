e66661020b22984fbd28c78816568f065c0b5e62
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-26 14:35:04 -0700

    chore: update hamt-dir test & kubo handling

--- test-e2e/hamt-dir.test.ts
+++ test-e2e/hamt-dir.test.ts
@@ -1,34 +1,8 @@
-import { test, expect } from '@playwright/test'
-import { createKuboNode } from './fixtures/create-kubo-node.js'
-import { loadFixtureDataCar } from './fixtures/load-fixture-data.js'
-import { setConfig } from './fixtures/set-sw-config.js'
-import { waitForServiceWorker } from './fixtures/wait-for-service-worker.js'
-import type { ConfigDb } from '../src/lib/config-db.js'
-import type { Controller } from 'ipfsd-ctl'
+import { testPathRouting as test, expect } from './fixtures/config-test-fixtures.js'
 
 test.describe('hamt-dir', () => {
-  let controller: Controller<'go'>
-  let config: Partial<ConfigDb>
-
-  test.beforeAll(async () => {
-    controller = await createKuboNode()
-    await controller.start()
-    await loadFixtureDataCar(controller, 'single-layer-hamt-with-multi-block-files.car')
-    config = {
-      gateways: [`http://${controller.api.gatewayHost}:${controller.api.gatewayPort}`],
-      routers: [`http://${controller.api.gatewayHost}:${controller.api.gatewayPort}`]
-    }
-  })
-
-  test.beforeEach(async ({ page }) => {
-    // not testing subdomain redirection here
-    await page.goto('http://127.0.0.1:3000', { waitUntil: 'networkidle' })
-    await setConfig({ page, config })
-    await waitForServiceWorker(page)
-  })
-
   test('can open UnixFS file from HAMT-sharded directory', async ({ page }) => {
-    const response = await page.goto('http://127.0.0.1:3000/ipfs/bafybeidbclfqleg2uojchspzd4bob56dqetqjsj27gy2cq3klkkgxtpn4i/685.txt')
+    const response = await page.goto('http://127.0.0.1:3333/ipfs/bafybeidbclfqleg2uojchspzd4bob56dqetqjsj27gy2cq3klkkgxtpn4i/685.txt')
 
     expect(response?.status()).toBe(200)
     const headers = await response?.allHeaders()
