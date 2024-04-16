2b7fee9ddea34e4cdc44ce6f21124351f0a45efc
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-16 11:59:34 -0700

    fix: regex support for more urls

--- test/regex.spec.ts
+++ test/regex.spec.ts
@@ -23,7 +23,19 @@ const validPathUrls = [
   ['http://example.com/ipns/specs.ipfs.tech', 'ipns', 'specs.ipfs.tech', ''],
   ['http://example.com/ipns/specs.ipfs.tech/', 'ipns', 'specs.ipfs.tech', '/'],
   ['http://example.com/ipns/specs.ipfs.tech/path/to/file', 'ipns', 'specs.ipfs.tech', '/path/to/file'],
-  ['http://example.com/ipns/specs.ipfs.tech/path/to/file/', 'ipns', 'specs.ipfs.tech', '/path/to/file/']
+  ['http://example.com/ipns/specs.ipfs.tech/path/to/file/', 'ipns', 'specs.ipfs.tech', '/path/to/file/'],
+
+  // schemeless
+  ['//example.com/ipns/specs.ipfs.tech', 'ipns', 'specs.ipfs.tech', ''],
+  ['//example.com/ipns/specs.ipfs.tech/', 'ipns', 'specs.ipfs.tech', '/'],
+  ['//example.com/ipns/specs.ipfs.tech/path/to/file', 'ipns', 'specs.ipfs.tech', '/path/to/file'],
+  ['//example.com/ipns/specs.ipfs.tech/path/to/file/', 'ipns', 'specs.ipfs.tech', '/path/to/file/'],
+
+  // with different path starts (hash, query, etc)
+  ['/ipfs/bafyFoo#hash', 'ipfs', 'bafyFoo', '#hash'],
+  ['/ipfs/bafyFoo?query', 'ipfs', 'bafyFoo', '?query'],
+  ['/ipfs/bafyFoo?query#hash', 'ipfs', 'bafyFoo', '?query#hash'],
+  ['/ipfs/bafyFoo#hash?query', 'ipfs', 'bafyFoo', '#hash?query']
 ]
 
 const validSubdomainUrls = [
@@ -40,7 +52,39 @@ const validSubdomainUrls = [
   // IPNS subdomains with dnslink
   ['http://specs-ipfs-tech.ipns.example.com', 'ipns', 'specs-ipfs-tech', ''],
   ['http://specs-ipfs-tech.ipns.example.com/', 'ipns', 'specs-ipfs-tech', '/'],
-  ['http://specs-ipfs-tech.ipns.example.com/path/to/file', 'ipns', 'specs-ipfs-tech', '/path/to/file']
+  ['http://specs-ipfs-tech.ipns.example.com/path/to/file', 'ipns', 'specs-ipfs-tech', '/path/to/file'],
+
+  // schemeless
+  ['//bafyFoo.ipfs.example.com', 'ipfs', 'bafyFoo', ''],
+  ['//bafyFoo.ipfs.example.com/', 'ipfs', 'bafyFoo', '/'],
+  ['//bafyFoo.ipfs.example.com/path/to/file', 'ipfs', 'bafyFoo', '/path/to/file'],
+
+  ['//bafyFoo.ipns.example.com', 'ipns', 'bafyFoo', ''],
+  ['//bafyFoo.ipns.example.com/', 'ipns', 'bafyFoo', '/'],
+  ['//bafyFoo.ipns.example.com/path/to/file', 'ipns', 'bafyFoo', '/path/to/file'],
+
+  ['//specs-ipfs-tech.ipns.example.com', 'ipns', 'specs-ipfs-tech', ''],
+  ['//specs-ipfs-tech.ipns.example.com/', 'ipns', 'specs-ipfs-tech', '/'],
+  ['//specs-ipfs-tech.ipns.example.com/path/to/file', 'ipns', 'specs-ipfs-tech', '/path/to/file'],
+
+  // hostname only
+  ['bafyFoo.ipfs.example.com', 'ipfs', 'bafyFoo', ''],
+  ['bafyFoo.ipfs.example.com/', 'ipfs', 'bafyFoo', '/'],
+  ['bafyFoo.ipfs.example.com/path/to/file', 'ipfs', 'bafyFoo', '/path/to/file'],
+
+  ['bafyFoo.ipns.example.com', 'ipns', 'bafyFoo', ''],
+  ['bafyFoo.ipns.example.com/', 'ipns', 'bafyFoo', '/'],
+  ['bafyFoo.ipns.example.com/path/to/file', 'ipns', 'bafyFoo', '/path/to/file'],
+
+  ['specs-ipfs-tech.ipns.example.com', 'ipns', 'specs-ipfs-tech', ''],
+  ['specs-ipfs-tech.ipns.example.com/', 'ipns', 'specs-ipfs-tech', '/'],
+  ['specs-ipfs-tech.ipns.example.com/path/to/file', 'ipns', 'specs-ipfs-tech', '/path/to/file'],
+
+  // with different path starts (hash, query, etc)
+  ['bafyFoo.ipfs.example.com#hash', 'ipfs', 'bafyFoo', '#hash'],
+  ['bafyFoo.ipfs.example.com?query', 'ipfs', 'bafyFoo', '?query'],
+  ['bafyFoo.ipfs.example.com?query#hash', 'ipfs', 'bafyFoo', '?query#hash'],
+  ['bafyFoo.ipfs.example.com#hash?query', 'ipfs', 'bafyFoo', '#hash?query']
 ]
 
 const validNativeProtocolUrls = [
