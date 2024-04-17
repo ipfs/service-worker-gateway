76ae50cdaa2500256beab38d38c2eddb32a6c87c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-17 10:30:55 -0700

    fix: helper-ui input label

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import React from 'preact/compat'
 
 export default ({ handleSubmit, requestPath, setRequestPath }): React.JSX.Element => (
   <form id='add-file' onSubmit={handleSubmit}>
-    <label htmlFor='inputContent' className='f5 ma0 pb2 aqua fw4 db'>CID (and path)</label>
+    <label htmlFor='inputContent' className='f5 ma0 pb2 aqua fw4 db'>CID, Content Path, or URL</label>
     <input
       className='input-reset bn black-80 bg-white pa3 w-100 mb3'
       id='inputContent'
