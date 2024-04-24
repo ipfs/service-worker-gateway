42babd05c9af6757de6d4f577e7aaae2345aeb2b
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-24 10:34:09 -0700

    chore: shorten debug log prefix

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,8 +2,8 @@ import { prefixLogger, type ComponentLogger } from '@libp2p/logger'
 
 const host = globalThis.location.host.replace(':', '_')
 
-const swLogPrefix = `helia:service-worker-gateway:sw:${host}`
-const uiLogPrefix = `helia:service-worker-gateway:ui:${host}`
+const swLogPrefix = `helia:sw-gateway:sw:${host}`
+const uiLogPrefix = `helia:sw-gateway:ui:${host}`
 
 export const swLogger = prefixLogger(swLogPrefix)
 export const uiLogger = prefixLogger(uiLogPrefix)
