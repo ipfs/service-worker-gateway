a873b7c93f1f6e14bb1d43e7bde7d00d71883856
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-06-05 11:41:35 -0700

    fix: ui updates to no-sw error page

--- src/pages/errors/no-service-worker.tsx
+++ src/pages/errors/no-service-worker.tsx
@@ -3,15 +3,22 @@
  */
 
 import React from 'react'
+import About from '../../components/About.jsx'
+import Header from '../../components/Header.jsx'
+import '../default-page-styles.css'
 
 export default function NoServiceWorkerErrorPage (): React.JSX.Element {
   return (
-    <div>
-      <h1>Service Worker Error</h1>
-      <p>
-        This page requires a service worker to be available. Please enable
-        service workers in your browser and try again.
-      </p>
-    </div>
+    <>
+      <Header />
+      <main className='pa4-l bg-red-muted mw7 mt1 mb5 center pa4'>
+        <h1>Service Worker Error</h1>
+        <p>
+          This page requires a service worker to be available. Please enable
+          service workers in your browser and try again.
+        </p>
+      </main>
+      <About />
+    </>
   )
 }
