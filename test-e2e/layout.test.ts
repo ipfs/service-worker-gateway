ddeb65fd203110d4eee0bc3ab51698adaba18d00
Daniel N <2color@users.noreply.github.com>
2024-06-20 16:25:05 +0200

    fix: tests

--- test-e2e/layout.test.ts
+++ test-e2e/layout.test.ts
@@ -31,7 +31,7 @@ test.describe('smoketests', () => {
       const inputLocator = getConfigPageInput(page)
       // see https://playwright.dev/docs/locators#strictness
       await inputLocator.first().waitFor()
-      expect(await inputLocator.count()).toEqual(5)
+      expect(await inputLocator.count()).toEqual(6)
       const submitButton = getConfigPageSaveButton(page)
       await expect(submitButton).toBeVisible()
     })
