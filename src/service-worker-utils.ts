8e7d15a515c8e411dd7cd114672d97ebed5439b6
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-22 17:16:34 -0500

    fix: firefox properly replaces old service worker

--- src/service-worker-utils.ts
+++ src/service-worker-utils.ts
@@ -8,29 +8,23 @@ async function supportsESMInServiceWorkers (): Promise<ServiceWorkerRegistration
       // don't use the cache for this registration
       updateViaCache: 'none'
     })
-    // await registration.unregister()
-    console.log('supportsESMInServiceWorkers', registration)
     return registration
   } catch (e) {
-    console.error('error', e)
     return null
   }
 }
+
 /**
  * This function is always and only used from the UI
  */
 export async function registerServiceWorker (): Promise<ServiceWorkerRegistration> {
   const log = uiLogger.forComponent('register-service-worker')
   const esmRegistration = await supportsESMInServiceWorkers()
-  console.log('esmRegistration', esmRegistration)
   const swToLoad = (esmRegistration != null) ? 'ipfs-sw-sw.js' : 'ipfs-sw-sw-es5.js'
   const swUrl = new URL(swToLoad, import.meta.url)
   log.trace('loading service worker', swToLoad)
   const currentRegistration = await navigator.serviceWorker.getRegistration()
   if (currentRegistration != null) {
-    currentRegistration.addEventListener('updatefound', () => {
-      console.log('update found 1')
-    })
     const currentScriptURL = currentRegistration.active?.scriptURL ?? currentRegistration.waiting?.scriptURL ?? currentRegistration.installing?.scriptURL
 
     // If the current service worker is different, unregister it
@@ -46,9 +40,7 @@ export async function registerServiceWorker (): Promise<ServiceWorkerRegistratio
 
   return new Promise((resolve, reject) => {
     swRegistration.addEventListener('updatefound', () => {
-      console.log('update found 2')
       const newWorker = swRegistration.installing
-      console.log('newWorker', newWorker)
       newWorker?.addEventListener('statechange', () => {
         if (newWorker.state === 'activated') {
           log('service worker activated')
