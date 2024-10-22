edc23a6a727c00e9ef8b8b531c50937a3efca9b9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-22 14:05:33 -0500

    fix: service worker logs are visible again

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,26 @@
-import { prefixLogger, type ComponentLogger } from '@libp2p/logger'
+import { type ComponentLogger, type Logger } from '@libp2p/logger'
+import debug from 'debug'
 
+/**
+ * Hack to get around @libp2p/logger not working inside a service worker.
+ */
+const createlibp2pLogger = (prefix: string): Logger => {
+  const mainLogger = debug(prefix)
+  return Object.assign(debug(prefix), {
+    error: mainLogger.extend('error'),
+    // warn: mainLogger.extend('warn'),
+    // info: mainLogger.extend('info'),
+    // debug: mainLogger.extend('debug'),
+    trace: mainLogger.extend('trace')
+  })
+}
+export function prefixLogger (prefix: string): ComponentLogger {
+  return {
+    forComponent (name: string) {
+      return createlibp2pLogger(`${prefix}:${name}`)
+    }
+  }
+}
 const host = globalThis.location.host.replace(':', '_')
 
 const swLogPrefix = `helia:sw-gateway:sw:${host}`
