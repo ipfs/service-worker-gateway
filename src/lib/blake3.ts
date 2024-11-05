2e3b6b088b0fcab44e172bad8193a0df2d1783f0
Daniel N <2color@users.noreply.github.com>
2024-11-05 15:57:22 +0100

    chore: add comment about code

--- src/lib/blake3.ts
+++ src/lib/blake3.ts
@@ -3,6 +3,6 @@ import { from } from 'multiformats/hashes/hasher'
 
 export const blake3 = from({
   name: 'blake3',
-  code: 0x1e,
+  code: 0x1e, // Code for blake3 from https://github.com/multiformats/multicodec/blob/352d05ad430713088e867216152725f581387bc8/table.csv#L21
   encode: (input) => b3(input)
 })
