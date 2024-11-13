36a96bc2ef49853bbcc821ebe696ef0a952e506a
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-11-13 16:17:14 -0600

    fix: input-toggle uses resetKey

--- src/components/input-toggle.tsx
+++ src/components/input-toggle.tsx
@@ -1,7 +1,7 @@
 /**
   Inspiration from https://dev.to/codebubb/create-a-simple-on-off-slide-toggle-with-css-db8
  */
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import './input-toggle.css'
 import { InputDescription } from './input-description.js'
 import { InputLabel } from './input-label.js'
@@ -24,6 +24,11 @@ export const InputToggle: React.FC<LocalStorageToggleProps> = ({
   onChange,
   ...props
 }) => {
+  const [internalValue, setInternalValue] = useState(value)
+
+  useEffect(() => {
+    setInternalValue(value)
+  }, [resetKey, value])
   /**
    * Lowercase and no spaces
    */
@@ -33,6 +38,7 @@ export const InputToggle: React.FC<LocalStorageToggleProps> = ({
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const newValue = event.target.checked
+    setInternalValue(newValue)
     onChange(newValue)
   }
 
@@ -44,19 +50,19 @@ export const InputToggle: React.FC<LocalStorageToggleProps> = ({
       <input
         type="checkbox"
         id={id}
-        checked={value}
+        checked={internalValue}
         onChange={handleChange}
         className="dn"
       />
       <label
         htmlFor={id}
         className={`relative dib h1 w2 flex-shrink-0 pointer br4 ${
-          value ? 'bg-green' : 'bg-dark-gray'
+          internalValue ? 'bg-green' : 'bg-dark-gray'
         } transition-all duration-200 ease-in-out`}
       >
         <span
           className={`absolute top-0 left-0 dib h1 w1 br-100 bg-white shadow-1 ${
-            value ? 'translate-x-100' : 'translate-x-0'
+            internalValue ? 'translate-x-100' : 'translate-x-0'
           } transition-transform duration-200 ease-in-out`}
         />
       </label>
