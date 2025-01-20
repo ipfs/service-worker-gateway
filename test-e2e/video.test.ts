b55e3ec02cf99b010ccb1411456b108f7afeed40
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-01-20 15:14:24 -0600

    fix: video auto-play test

--- test-e2e/video.test.ts
+++ test-e2e/video.test.ts
@@ -1,10 +1,10 @@
-import { test, expect } from './fixtures/config-test-fixtures.js'
+import { testPathRouting as test, expect } from './fixtures/config-test-fixtures.js'
 import { setConfig } from './fixtures/set-sw-config.js'
 import { waitForServiceWorker } from './fixtures/wait-for-service-worker'
 import type { ConfigDbWithoutPrivateFields } from '../src/lib/config-db.js'
 
-// const cid = 'bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4' // big buck bunny webm trimmed to 15 seconds with `ffmpeg -i bigbuckbunny.webm -ss 00:00 -t 00:15 -c:a copy -c:v copy bigbuckbunny-mini.webm`
-const cid = 'bafkreicfnb5pizrilmrcuarbihfwdha6bz26veg526d7r2hwtz5qechvcy' // first CID when running `npx kubo@latest ls bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4`
+const cid = 'bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4' // big buck bunny webm trimmed to 15 seconds with `ffmpeg -i bigbuckbunny.webm -ss 00:00 -t 00:15 -c:a copy -c:v copy bigbuckbunny-mini.webm`
+
 test.describe('video', () => {
   const testConfig: Partial<ConfigDbWithoutPrivateFields> = {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -15,35 +15,20 @@ test.describe('video', () => {
     enableWss: true,
     enableWebTransport: false,
     enableRecursiveGateways: true,
-    enableGatewayProviders: false
+    enableGatewayProviders: true
   }
 
   /**
    * We want to load the video fixture and ensure it starts playing.
    */
-  test('starts playing automatically', async ({ page, protocol, rootDomain }) => {
-    await page.goto(`${protocol}//${rootDomain}`)
+  test('starts playing automatically', async ({ page }) => {
     await setConfig({ page, config: testConfig })
     await waitForServiceWorker(page)
-    // let serviceWorkerConfigJson = await page.evaluate(async () => {
-    //   const response = await fetch('/#/ipfs-sw-config-get')
-    //   return response.json()
-    // })
-    // expect(serviceWorkerConfigJson).toMatchObject(testConfig)
-    const response = await page.goto(`${protocol}//${rootDomain}/ipfs/${cid}`, { waitUntil: 'commit' })
+    const response = await page.goto(`http://127.0.0.1:3333/ipfs/${cid}`, { waitUntil: 'commit' })
     const start = performance.now()
 
     expect(response?.status()).toBe(200)
     await waitForServiceWorker(page)
-
-    // sleep for a few seconds
-    // await page.waitForTimeout(2000)
-
-    // serviceWorkerConfigJson = await page.evaluate(async () => {
-    //   const response = await fetch('/#/ipfs-sw-config-get')
-    //   return response.json()
-    // })
-    // expect(serviceWorkerConfigJson).toMatchObject(testConfig)
 
     // expect a video player
     await page.waitForSelector('video')
