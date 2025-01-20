7d0749e1017a09300737352dcfa74e28341c5a32
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-01-20 14:55:32 -0600

    chore: trying to get video test passing again....

--- test-e2e/video.test.ts
+++ test-e2e/video.test.ts
@@ -1,9 +1,10 @@
-import { testPathRouting as test, expect } from './fixtures/config-test-fixtures.js'
+import { test, expect } from './fixtures/config-test-fixtures.js'
 import { setConfig } from './fixtures/set-sw-config.js'
 import { waitForServiceWorker } from './fixtures/wait-for-service-worker'
 import type { ConfigDbWithoutPrivateFields } from '../src/lib/config-db.js'
 
-const cid = 'bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4' // big buck bunny webm trimmed to 15 seconds with `ffmpeg -i bigbuckbunny.webm -ss 00:00 -t 00:15 -c:a copy -c:v copy bigbuckbunny-mini.webm`
+// const cid = 'bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4' // big buck bunny webm trimmed to 15 seconds with `ffmpeg -i bigbuckbunny.webm -ss 00:00 -t 00:15 -c:a copy -c:v copy bigbuckbunny-mini.webm`
+const cid = 'bafkreicfnb5pizrilmrcuarbihfwdha6bz26veg526d7r2hwtz5qechvcy' // first CID when running `npx kubo@latest ls bafybeie4vcqkutumw7s26ob2bwqwqi44m6lrssjmiirlhrzhs2akdqmkw4`
 test.describe('video', () => {
   const testConfig: Partial<ConfigDbWithoutPrivateFields> = {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -24,10 +25,25 @@ test.describe('video', () => {
     await page.goto(`${protocol}//${rootDomain}`)
     await setConfig({ page, config: testConfig })
     await waitForServiceWorker(page)
+    // let serviceWorkerConfigJson = await page.evaluate(async () => {
+    //   const response = await fetch('/#/ipfs-sw-config-get')
+    //   return response.json()
+    // })
+    // expect(serviceWorkerConfigJson).toMatchObject(testConfig)
     const response = await page.goto(`${protocol}//${rootDomain}/ipfs/${cid}`, { waitUntil: 'commit' })
     const start = performance.now()
 
     expect(response?.status()).toBe(200)
+    await waitForServiceWorker(page)
+
+    // sleep for a few seconds
+    // await page.waitForTimeout(2000)
+
+    // serviceWorkerConfigJson = await page.evaluate(async () => {
+    //   const response = await fetch('/#/ipfs-sw-config-get')
+    //   return response.json()
+    // })
+    // expect(serviceWorkerConfigJson).toMatchObject(testConfig)
 
     // expect a video player
     await page.waitForSelector('video')
