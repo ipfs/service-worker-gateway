3248ae006225470e6471ede9ff2dd78d583c66fb
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-04-09 09:54:43 -0500

    test: validate dir-index-html text and links

--- test-e2e/smoke-test.test.ts
+++ test-e2e/smoke-test.test.ts
@@ -24,4 +24,35 @@ test.describe('smoke test', () => {
     expect(response?.headers()['content-type']).toBe('text/html; charset=utf-8')
     expect(await response?.text()).toContain('hello')
   })
+
+  /**
+   * TODO: address issues mentioned in https://github.com/ipfs/helia-verified-fetch/issues/208
+   */
+  test('request to /ipfs/dir-cid without index.html returns dir listing', async ({ page, protocol, swResponses }) => {
+    await navigateAndGetSwResponse(page, {
+      url: `${protocol}//localhost:3334/ipfs/bafybeib3ffl2teiqdncv3mkz4r23b5ctrwkzrrhctdbne6iboayxuxk5ui/root2/root3`,
+      swScope: 'http://bafybeib3ffl2teiqdncv3mkz4r23b5ctrwkzrrhctdbne6iboayxuxk5ui.ipfs.localhost:3334'
+    })
+    await page.waitForURL(`${protocol}//bafybeib3ffl2teiqdncv3mkz4r23b5ctrwkzrrhctdbne6iboayxuxk5ui.ipfs.localhost:3334/root2/root3/`)
+    const response = swResponses[swResponses.length - 1]
+    expect(response?.status()).toBe(200)
+    expect(response?.headers()['content-type']).toBe('text/html')
+    // there should be a dir listing that has the CID of the root3 node, and the name of the root4 node with a short name
+
+    const header = await page.waitForSelector('main header')
+    expect(await header?.innerText()).toContain('Index of root3')
+    const headerCid = await header.$('.ipfs-hash')
+    expect(await headerCid?.innerText()).toContain('bafybeiawdvhmjcz65x5egzx4iukxc72hg4woks6v6fvgyupiyt3oczk5ja')
+
+    // .grid.dir will have a list of rows (1 in this case) with links to the files in the directory. One link where the name is, and one link with a short hash (.ipfs-hash)
+    const gridDir = await page.waitForSelector('.grid.dir')
+    const rowCells = await gridDir?.$$('> div') // ideally we would have a better selector for each row, but there is currently no wrapping element for each row
+    expect(rowCells?.length).toBe(4)
+    const nameLink = await rowCells[1].$('a')
+    const shortHashLink = await rowCells[2].$('a')
+    expect(await nameLink?.innerText()).toBe('root4')
+    expect(await nameLink?.getAttribute('href')).toBe('root4')
+    expect(await shortHashLink?.innerText()).toContain('bafy...lo7q')
+    expect(await shortHashLink?.getAttribute('href')).toContain('http://localhost:3334/ipfs/bafybeifq2rzpqnqrsdupncmkmhs3ckxxjhuvdcbvydkgvch3ms24k5lo7q?filename=root4')
+  })
 })
