3443e61adf0b065f37fece9c258988bf4b3a1726
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-04-10 10:06:20 -0500

    refactor: ipfs-gateway.js -> ipfs-gateway.ts

--- test-e2e/ipfs-gateway.ts
+++ test-e2e/ipfs-gateway.ts
@@ -4,17 +4,19 @@
  */
 import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import { tmpdir } from 'node:os'
-import { dirname, join, relative } from 'node:path'
+import { dirname, join, relative, resolve } from 'node:path'
 import { cwd } from 'node:process'
 import { fileURLToPath } from 'node:url'
 import { logger } from '@libp2p/logger'
 import { $, execa } from 'execa'
+import { glob } from 'glob'
 import { path } from 'kubo'
 import { createReverseProxy } from './reverse-proxy.js'
 
 const log = logger('ipfs-host.local')
 const daemonLog = logger('ipfs-host.local:kubo')
 const proxyLog = logger('ipfs-host.local:proxy')
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const tempDir = tmpdir()
 const IPFS_PATH = `${tempDir}/.ipfs/${Date.now()}`
@@ -50,8 +52,9 @@ log('using IPFS_PATH: ', IPFS_PATH)
 const kuboRedirects = await readFile(join(cwd(), './dist/_kubo_redirects'), 'utf-8')
 
 await writeFile(join(cwd(), './dist/_redirects'), kuboRedirects)
-
-const { stdout: cid } = await $(execaOptions)`${kuboBin} add -r -Q ${relative(cwd(), '../dist')} --cid-version 1`
+const distPath = relative(cwd(), '../../dist')
+console.log('distPath: ', join(__dirname, distPath))
+const { stdout: cid } = await $(execaOptions)`${kuboBin} add -r -Q ${distPath} --cid-version 1`
 
 log('sw-gateway dist CID: ', cid.trim())
 const { stdout: pinStdout } = await $(execaOptions)`${kuboBin} pin add -r /ipfs/${cid.trim()}`
@@ -73,11 +76,17 @@ await $(execaOptions)`${kuboBin} config --json Gateway.ExposeRoutingAPI false`
 await $(execaOptions)`${kuboBin} config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin ${JSON.stringify(['*'])}`
 await $(execaOptions)`${kuboBin} config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods  ${JSON.stringify(['GET', 'POST', 'PUT', 'OPTIONS'])}`
 
+for (const carFile of await glob([`${resolve(__dirname, 'fixtures', 'data')}/**/*.car`])) {
+  log('Loading *.car fixture %s', carFile)
+  const { stdout } = await $(execaOptions)`${kuboBin} dag import --pin-roots=false --offline ${carFile}`
+  stdout.split('\n').forEach(log)
+}
+
 log('starting kubo')
 // need to stand up kubo daemon to serve the dist folder
 const daemon = execa(kuboBin, ['daemon', '--offline'], execaOptions)
 
-if (daemon == null || (daemon.stdout == null || daemon.stderr == null)) {
+if (daemon?.stdout == null || daemon.stderr == null) {
   throw new Error('failed to start kubo daemon')
 }
 daemon.stdout.on('data', (data) => {
@@ -89,7 +98,7 @@ daemon.stderr.on('data', (data) => {
 
 // check for "daemon is ready" message
 await new Promise((resolve, reject) => {
-  daemon.stdout?.on('data', (data) => {
+  daemon.stdout?.on('data', (data: string) => {
     if (data.includes('Daemon is ready')) {
       // @ts-expect-error - nothing needed here.
       resolve()
