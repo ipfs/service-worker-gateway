8386e83f3390d568a6e1c19991f3e8919e54b21d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-04-15 11:27:25 -0500

    chore: remove navigateAndGetSwResponse

--- test-e2e/global-setup.ts
+++ test-e2e/global-setup.ts
@@ -1,7 +1,11 @@
+import { enable } from '@libp2p/logger'
 import { type Config } from '@playwright/test'
 import { serve } from '../serve.js'
 
 export default async function globalSetup (config: Config): Promise<void> {
+  if (process.env.CI === 'true') {
+    enable('kubo-init*,kubo-init*:trace,ipfs-host.local*,ipfs-host.local*:trace')
+  }
   process.env.PLAYWRIGHT = 'true'
 
   const { controller } = await serve({ shouldLoadFixtures: true, shouldStartFrontend: false })
