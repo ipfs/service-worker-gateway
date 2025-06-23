b5554ae8a0612a3be7d05b03f81c942032c774cd
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-06-23 12:57:23 -0400

    fix: config timestamp is broad enough for 3g loading

--- src/lib/config-db.ts
+++ src/lib/config-db.ts
@@ -258,8 +258,8 @@ export async function decompressConfig (compressedConfig: string): Promise<Confi
     throw new Error('Attempted to decompress config from an untrusted URL.')
   }
   const { config, timestamp } = JSON.parse(LZString.decompressFromEncodedURIComponent(compressedConfig))
-  // if the config is more than 1 second old, throw an error
-  if (Date.now() - timestamp > 1000) {
+  // if the config is more than 15 seconds old (allow for 3g latency), throw an error
+  if (Date.now() - timestamp > 15000) {
     throw new Error('Config is too old. Be sure to only use trusted URLs.')
   }
   return config
