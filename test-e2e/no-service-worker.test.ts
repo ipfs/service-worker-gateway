98c85ebc9350fa3a792e4e7b219409edaa0d8ae4
achingbrain <alex@achingbrain.net>
2025-11-08 16:45:31 +0200

    chore: wat

--- test-e2e/no-service-worker.test.ts
+++ test-e2e/no-service-worker.test.ts
@@ -2,13 +2,13 @@ import { testNoServiceWorker as test, expect } from './fixtures/config-test-fixt
 import { getNoServiceWorkerError } from './fixtures/locators.js'
 
 test.describe('no-service-worker', () => {
-  test('Error renders on landing page', async ({ page }) => {
+  test('warning message renders on landing page', async ({ page }) => {
     await page.goto('/')
 
     await expect(getNoServiceWorkerError(page)).toBeVisible()
   })
 
-  test('Error renders on subdomain page', async ({ page, rootDomain, protocol }) => {
+  test('warning message renders on subdomain page', async ({ page, rootDomain, protocol }) => {
     await page.goto(`${protocol ?? 'http'}//bafkqablimvwgy3y.ipfs.${rootDomain}`, { waitUntil: 'networkidle' })
 
     await expect(getNoServiceWorkerError(page)).toBeVisible()
