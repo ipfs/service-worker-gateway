d1a6c674a61a0bf049104c4564a2595fec6a3793
Alex Potsides <alex@achingbrain.net>
2026-04-09 13:33:41 +0200

    fix: improve error message when streaming fails (#1021)
    When a provider claims to have the block but then fails to stream it,
    show a better error message.

--- src/ui/pages/fetch-error.tsx
+++ src/ui/pages/fetch-error.tsx
@@ -240,6 +240,7 @@ export function FetchErrorPage ({ request, response, logs, providers }: FetchErr
     message = (
       <>
         <p className='f5 ma3 fw4 db'>The content was loaded but could not be processed.</p>
+        <p className='f5 ma3 fw4 db'>This may mean that a recursive gateway failed to fetch the content on our behalf.</p>
         <Terminal>{response.body}</Terminal>
         {whatNextMessage}
       </>
